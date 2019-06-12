ba19072e18a925859ae2e9ca7a3808778ae1040b
Mateo Lopez Espejo <lopezesp@ohsu.edu>
2019-06-12 15:46:55 -0700

    modified pathing to load and save preprocessed data and results to elephant

--- code/root_directory.m
+++ code/root_directory.m
@@ -4,8 +4,8 @@ if exist('/home/jennifer', 'dir')
     y = '/home/jennifer/sam';
 elseif exist('/home/sam', 'dir')
     y = '/home/sam';
-elseif exist('/home/mateo/Sam Analysis', 'dir') % MLE 
-    y = '/home/mateo/Sam Analysis';
+elseif exist('/auto/users/mateo/Sam Analysis', 'dir') % MLE 
+    y = '/auto/users/mateo/Sam Analysis';
 else
     x = dir('/');
     s = {x(:).name};
